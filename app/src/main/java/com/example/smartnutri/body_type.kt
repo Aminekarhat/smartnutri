11c46131d0c2d7e85ee2028272f8484e1de89ce5
Aminekarhat <karhat.ptech2020@gmail.com>
2022-08-06 12:55:31 +0100

    first_page_done

---
+++ app/src/main/java/com/example/smartnutri/body_type.kt
@@ -0,0 +1,11 @@
+package com.example.smartnutri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class body_type : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_body_type)
+    }
+}
