2486032943c2311f25d82aa677a896344389d966
Aminekarhat <karhat.ptech2020@gmail.com>
2022-08-08 10:52:14 +0100

    second_page_done

--- app/src/main/java/com/example/smartnutri/body_type.kt
+++ app/src/main/java/com/example/smartnutri/body_type.kt
@@ -1,11 +1,30 @@
 package com.example.smartnutri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class body_type : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_body_type)
+        val fatt=findViewById<Button>(R.id.button3)
+        fatt.setOnClickListener{
+            val intent = Intent(this,fat::class.java)
+            startActivity(intent)
+        }
+
+        val cutt=findViewById<Button>(R.id.button5)
+        cutt.setOnClickListener{
+            val intent = Intent(this,cut::class.java)
+            startActivity(intent)
+        }
+
+        val bulkk=findViewById<Button>(R.id.button4)
+        bulkk.setOnClickListener{
+            val intent = Intent(this,bulk::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/smartnutri/cut.kt
@@ -0,0 +1,11 @@
+package com.example.smartnutri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cut : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cut)
+    }
+}
