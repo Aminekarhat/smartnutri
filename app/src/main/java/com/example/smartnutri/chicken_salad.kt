4585e757e69494c86cb15423d3b55ba5815e4f54
Aminekarhat <karhat.ptech2020@gmail.com>
2022-08-11 16:25:26 +0100

    add fourth_page

--- app/src/main/java/com/example/smartnutri/chicken_salad.kt
+++ app/src/main/java/com/example/smartnutri/chicken_salad.kt
@@ -1,11 +1,15 @@
 package com.example.smartnutri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.ImageButton
 class chicken_salad : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chicken_salad)
+
     }
 }
--- app/src/main/java/com/example/smartnutri/cut.kt
+++ app/src/main/java/com/example/smartnutri/cut.kt
@@ -1,11 +1,23 @@
 package com.example.smartnutri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
 
 class cut : AppCompatActivity() {
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cut)
+       val botona = findViewById<ImageButton>(R.id.imageButton9)
+        botona.setOnClickListener{
+            val intent = Intent(this,chicken_salad::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
