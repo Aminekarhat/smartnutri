4ba955368ff6e84b0fb81c916dfa6e35bb4c0a66
Aminekarhat <karhat.ptech2020@gmail.com>
2022-08-11 17:08:29 +0100

    add 80% project

--- app/src/main/java/com/example/smartnutri/cut.kt
+++ app/src/main/java/com/example/smartnutri/cut.kt
@@ -17,6 +17,16 @@ class cut : AppCompatActivity() {
             val intent = Intent(this,chicken_salad::class.java)
             startActivity(intent)
         }
+        val botona2 = findViewById<ImageButton>(R.id.imageButton)
+        botona2.setOnClickListener{
+            val intent = Intent(this,vegeta::class.java)
+            startActivity(intent)
+        }
+        val botona3 = findViewById<ImageButton>(R.id.imageButton2)
+        botona3.setOnClickListener{
+            val intent = Intent(this,beef::class.java)
+            startActivity(intent)
+        }
 
     }
 
---
+++ app/src/main/java/com/example/smartnutri/vegeta.kt
@@ -0,0 +1,15 @@
+package com.example.smartnutri
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+class vegeta : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vegeta)
+
+    }
+}
