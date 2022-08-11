d4f360c4b755a62ea229fdabd097b0ad0bc6041c
Aminekarhat <karhat.ptech2020@gmail.com>
2022-08-11 14:47:18 +0100

    add fourth_page

---
+++ app/src/main/java/com/example/smartnutri/chicken_salad.kt
@@ -0,0 +1,11 @@
+package com.example.smartnutri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chicken_salad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chicken_salad)
+    }
+}
